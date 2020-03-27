7de1da77a037fdd2336fb47c5a03e44c6a5cf9e5
TanyaAzart <tazarchenko@mail.ru>
2020-03-27 22:10:03 +0100

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 connectDB()
@@ -10,7 +11,14 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use('/api/auth', require ('./routes/auth'))
 
-app.get('/', (req, res)=>res.send('Hello World!'))
+if(process.env.NODE_ENV==='production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
